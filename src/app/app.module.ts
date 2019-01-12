880013a0e9626261910832a8cfaeb8ebf52ef47d
smalldog124 <panumars124@gmail.com>
2019-01-12 11:28:02 +0700

    added:>เพิ่มการ route ไปยังหน้า add-coustomer-service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { AddCustomerStoresComponent } from './add-customer-stores/add-customer-stores.component';
@@ -7,6 +8,10 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './material';
 import { MainNavComponent } from './main-nav/main-nav.component';
 import { LayoutModule } from '@angular/cdk/layout';
+
+const appRoutes: Routes=[
+  {path:'addCoustomerShop',component:AddCustomerStoresComponent}
+]
 @NgModule({
   declarations: [
     AppComponent,
@@ -17,7 +22,8 @@ import { LayoutModule } from '@angular/cdk/layout';
     BrowserModule,
     BrowserAnimationsModule,
     LayoutModule,
-    MaterialModule
+    MaterialModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
